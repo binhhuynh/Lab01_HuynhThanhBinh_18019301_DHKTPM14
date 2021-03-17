03f8bd1d4240909e3686860e6c3852f89834755a
Thanh Bình Huỳnh <huynhthanhbinh290@gmail.com>
2021-03-17 21:47:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab01_huynhthanhbinh_18019301_dhktpm14/Bai_1d.java
@@ -0,0 +1,14 @@
+package com.example.lab01_huynhthanhbinh_18019301_dhktpm14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bai_1d extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai_1d);
+    }
+}
