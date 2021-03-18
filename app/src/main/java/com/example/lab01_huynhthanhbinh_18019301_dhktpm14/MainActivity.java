7a8cc06b9b87f91951c7c8cd71ff39da27152473
Thanh Bình Huỳnh <huynhthanhbinh290@gmail.com>
2021-03-18 17:54:34 +0700

    update all layout

--- app/src/main/java/com/example/lab01_huynhthanhbinh_18019301_dhktpm14/MainActivity.java
+++ app/src/main/java/com/example/lab01_huynhthanhbinh_18019301_dhktpm14/MainActivity.java
@@ -9,6 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_bai_1b);
+        setContentView(R.layout.activity_bai_1c);
+//        setContentView(R.layout.activity_bai_1d);
+//        setContentView(R.layout.activity_bai_1e);
+
     }
 }
